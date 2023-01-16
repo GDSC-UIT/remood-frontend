acaba7c16fa7dc554fbabc260a5d6e15a15dd613
Dat Duong <duongquangdat172004@gmail.com>
2023-01-16 16:13:15 +0700

    write diary screen 1

---
+++ lib/app/modules/diary/widgets/stack_topic.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:remood/app/core/values/app_colors.dart';
+
+class StackTopic extends StatelessWidget {
+  const StackTopic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width;
+    double _screenHeight = MediaQuery.of(context).size.height;
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          width: _screenWidth * 0.731,
+          height: _screenHeight * 0.115,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: AppColors.brown)),
+        ),
+        Positioned(
+          top: -30,
+          left: -26,
+          child: Container(
+            width: _screenWidth * 0.224,
+            height: _screenHeight * 0.061,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: AppColors.brown),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
