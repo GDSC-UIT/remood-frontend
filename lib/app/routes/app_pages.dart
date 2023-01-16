9cae74a9d3629b0f198c963ae6a3a565babceff5
Dat Duong <duongquangdat172004@gmail.com>
2023-01-16 14:30:11 +0700

    add write diary

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:remood/app/modules/diary/diary_binding.dart';
+import 'package:remood/app/modules/diary/screens/write_diary_screen.dart';
 import '/app/modules/home/home_binding.dart';
 import '/app/modules/home/screens/home_screen.dart';
 import '/app/routes/app_routes.dart';
@@ -10,5 +12,10 @@ abstract class AppPages {
       page: () => const HomeScreen(),
       binding: HomeBinding(),
     ),
+    GetPage(
+      name: AppRoutes.diary,
+      page: () => const DiaryScreen(),
+      binding: DiaryBinding(),
+    )
   ];
 }
