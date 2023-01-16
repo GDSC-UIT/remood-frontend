7cb66b0a0481b846469b3ace412e9513fd38a857
Dat Duong <duongquangdat172004@gmail.com>
2023-01-16 11:48:10 +0700

    Bottonm navigation bar

--- lib/app/core/values/app_colors.dart
+++ lib/app/core/values/app_colors.dart
@@ -3,5 +3,5 @@ import 'package:get/get.dart';
 
 class AppColors {
   AppColors._();
-  static const Color homeBackgroundColor = Color.fromRGBO(241, 239, 235, 1);
+  static const Color BackgroundColor = Color.fromARGB(255, 199, 196, 190);
 }
--- lib/app/modules/home/screens/home_screen.dart
+++ lib/app/modules/home/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:remood/app/core/values/app_colors.dart';
+import 'package:remood/app/global_widgets/bottombar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,8 +10,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     double _screenWidth = MediaQuery.of(context).size.width;
     double _screenHeight = MediaQuery.of(context).size.height;
-    return const Scaffold(
-      backgroundColor: AppColors.homeBackgroundColor,
+    return Scaffold(
+      backgroundColor: AppColors.BackgroundColor,
+      bottomNavigationBar: SizedBox(
+        height: _screenHeight * 0.11,
+        child: const ClipRRect(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+          child: BottomNavigation(),
+        ),
+      ),
     );
   }
 }
