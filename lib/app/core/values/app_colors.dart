a1476b31816b02722e7e287ac959c4df11e2515c
Dat Duong <duongquangdat172004@gmail.com>
2023-01-16 13:58:10 +0700

    fix color

--- lib/app/core/values/app_colors.dart
+++ lib/app/core/values/app_colors.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 
 class AppColors {
   AppColors._();
-  static const Color BackgroundColor = Color.fromARGB(255, 199, 196, 190);
+  static const Color BackgroundColor = Color.fromARGB(255, 244, 230, 204);
   static const Color MainColor = Color.fromRGBO(250, 173, 20, 1);
   static const Color brown = Color.fromRGBO(143, 117, 63, 1);
 }
