c5a73ccffc5fe59a9212079c43e46ce1510b7bc1
Kien_Ta <tathuctrungkien2003@gmail.com>
2023-01-18 21:01:54 +0700

    Fix 'Unable to load assets' of onboarding screen

--- lib/app/core/values/assets_images.dart
+++ lib/app/core/values/assets_images.dart
@@ -1,8 +1,8 @@
 class Assets {
   Assets._();
   // Icons
-  static const String smile = 'assets/icons/Smile.png';
+  static const String smile = 'assets/Smile.png';
 
   // Images
-  static const String onboarding1 = 'assets/images/onboarding1.jpg';
+  static const String onboarding1 = 'assets/onboarding1.jpg';
 }
