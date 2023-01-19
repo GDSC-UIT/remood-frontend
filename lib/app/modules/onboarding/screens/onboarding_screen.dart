c9580b19e5046ec333cb1941a498d2a73b7c6590
Kien_Ta <tathuctrungkien2003@gmail.com>
2023-01-19 08:18:21 +0700

    Refactor onboarding code

--- lib/app/modules/onboarding/screens/onboarding_screen.dart
+++ lib/app/modules/onboarding/screens/onboarding_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:remood/app/core/values/app_colors.dart';
 import 'package:remood/app/core/values/assets_images.dart';
 import 'package:remood/app/core/values/text_style.dart';
+import 'package:remood/app/modules/onboarding/widgets/onboarding_button.dart';
 import 'package:remood/app/modules/onboarding/widgets/onboarding_decoration.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -19,6 +20,7 @@ class OnboardingScreen extends StatelessWidget {
               child: PageView.builder(
                 itemCount: 3,
                 itemBuilder: (_, i) {
+                  // TODO: Tạo widget Onboarding content
                   return Container(
                     decoration: OnboardingDecoration.imageBackround(
                         Assets.onboardingBackground1),
@@ -26,37 +28,28 @@ class OnboardingScreen extends StatelessWidget {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Column(
-                          children: [
-                            // Background
-                            Image.asset(Assets.onboardingImage1),
-                            // Paragraph
-                            Text(
-                              "This is a paragraph. This is a paragraph. This is a paragraph. "
-                              "This is a paragraph. This is a paragraph. This is a paragraph. "
-                              "This is a paragraph. This is a paragraph. This is a paragraph. "
-                              "This is a paragraph. This is a paragraph. ",
-                              style: CustomTextStyle.onboardingText(
-                                  AppColors.primary),
-                            ),
-                          ],
+                        Image.asset(Assets.onboardingImage1),
+                        // Paragraph
+                        Text(
+                          "This is a paragraph. This is a paragraph. This is a paragraph. "
+                          "This is a paragraph. This is a paragraph. This is a paragraph. "
+                          "This is a paragraph. This is a paragraph. This is a paragraph. "
+                          "This is a paragraph. This is a paragraph. ",
+                          style:
+                              CustomTextStyle.onboardingText(AppColors.primary),
                         ),
-                        // Dots
-
-                        // Next screen
                       ],
                     ),
                   );
                 },
               ),
             ),
-            SizedBox(
-              width: 300,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text("Next"),
-              ),
-            ),
+
+            // todo: Dots
+            // Row
+            // List.generate
+            // Container
+            const OnboardingButton(),
           ],
         ),
       ),
---
+++ lib/app/modules/onboarding/widgets/onboarding_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class OnboardingButton extends StatelessWidget {
+  const OnboardingButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      // todo: Create button by Container
+      width: 300,
+      // Next screen
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text("Next"),
+      ),
+    );
+  }
+}
