9eedcd42dc1201731272e9243986f19581a7e1ed
Kien_Ta <tathuctrungkien2003@gmail.com>
2023-01-27 11:05:08 +0700

    Update asset_image.dart

--- lib/app/core/values/assets_images.dart
+++ lib/app/core/values/assets_images.dart
@@ -1,16 +1,19 @@
 class Assets {
   Assets._();
   // Icons
-  static const String smile = 'assets/Smile.png';
-  static const String narrowBack = 'assets/arrow-back.png';
+  static const String smile = 'assets/icons/smile.png';
+  static const String narrowBack = 'assets/icons/arrow-back.png';
 
   // Images
-  static const String onboardingBackground = 'assets/onboarding_bg.jpg';
-  static const String onboardingBackground1 = 'assets/onboarding_bg1.jpg';
-  static const String onboardingBackground2 = 'assets/onboarding_bg2.jpg';
-  static const String onboardingBackground3 = 'assets/onboarding_bg3.jpg';
-  static const String onboardingImage1 = 'assets/onboarding_image1.png';
-  static const String onboardingImage2 = 'assets/onboarding_image2.png';
-  static const String onboardingImage3 = 'assets/onboarding_image3.png';
-  static const String onboardingAlarmImage = 'assets/alarm.jpg';
+  static const String onboardingBackground = 'assets/images/onboarding_bg.jpg';
+  static const String onboardingBackground1 =
+      'assets/images/onboarding_bg1.jpg';
+  static const String onboardingBackground2 =
+      'assets/images/onboarding_bg2.jpg';
+  static const String onboardingBackground3 =
+      'assets/images/onboarding_bg3.jpg';
+  static const String onboardingImage1 = 'assets/images/onboarding_image1.png';
+  static const String onboardingImage2 = 'assets/images/onboarding_image2.png';
+  static const String onboardingImage3 = 'assets/images/onboarding_image3.png';
+  static const String onboardingAlarmImage = 'assets/images/alarm.jpg';
 }
