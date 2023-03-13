89308614c4fbf43bce735bb7ac58044b4d76ba34
Kien_Ta <tathuctrungkien2003@gmail.com>
2023-03-13 23:28:02 +0700

    Update suggestion button

--- lib/app/modules/report/widgets/button.dart
+++ lib/app/modules/report/widgets/button.dart
@@ -18,18 +18,20 @@ class Button extends StatelessWidget {
         Get.toNamed(AppRoutes.suggestion);
       },
       child: Container(
-          width: _screenWidth * 0.517,
-          height: _screenHeight * 0.058,
-          decoration: BoxDecoration(
-              color: AppColors.mainColor,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [BoxShadow(offset: Offset(0, 1))]),
-          child: Center(
-            child: Text(
-              "Remood’s suggestion",
-              style: CustomTextStyle.suggestionButtonText(),
-            ),
-          )),
+        width: _screenWidth * 0.517,
+        height: _screenHeight * 0.058,
+        decoration: BoxDecoration(
+            color: AppColors.buttonDiary,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [BoxShadow(offset: Offset(0, 1))]),
+        child: Center(
+          child: Text(
+            "Remood’s suggestion",
+            style: CustomTextStyle.suggestionButtonText()
+                .copyWith(color: AppColors.primary),
+          ),
+        ),
+      ),
     );
   }
 }
